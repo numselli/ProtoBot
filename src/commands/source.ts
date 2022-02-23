7cfbfae5817ae22813c3e6dc229f84b2198086bc
thetayloredman <nutdriver716@gmail.com>
2022-02-22 20:39:15 -0800

    e

--- src/commands/source.ts
+++ src/commands/source.ts
@@ -23,14 +23,14 @@ function escapeMarkdown(text: string) {
 }
 
 // Modules
-import type { Client, Message } from 'discord.js';
+import type { Client, Message, Collection } from 'discord.js';
 import type Logger from '@lib/interfaces/Logger';
 
 // Main
 export async function run(client: Client, message: Message, args: string[], log: Logger): Promise<void> {
-    const messages = await message.channel.messages.fetch({ limit: 2 });
+    const messages = await message.channel.messages.fetch({ limit: 2 }) as Collection<string, Message>;
 
-    const m: Message = messages.last();
+    const m: Message = messages.last() as Message;
     message.reply(`Content of message ID \`${m.id}\` in channel <#${m.channel.id}>:\n\n${escapeMarkdown(m.content)}`);
 }
 

47823a1fe99f998c2e72bca09bf93c0f304e518b
0xLogN <nutdriver716@gmail.com>
2022-04-12 11:45:37 -0700

    feat(help): only show "owner" category if you are the owner

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -53,6 +53,9 @@ export async function run(client: Client, message: Message, args: string[], log:
 
         // Add each category to the embed
         commandsInCategory.forEach((category) => {
+            if (category[0] === 'owner' && message.author.id === client.config.ownerID) embed.addField(category[0], category[1].join(', '));
+            else return;
+
             embed.addField(category[0].toString(), category[1].join(', '), true);
         });
 

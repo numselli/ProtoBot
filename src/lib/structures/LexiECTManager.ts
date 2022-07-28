8daa175845c0bab0297bf4de4de136564d469560
0xLogN <nutdriver716@gmail.com>
2022-07-28 12:56:16 -0700

    feat: reverse??

--- src/lib/structures/LexiECTManager.ts
+++ src/lib/structures/LexiECTManager.ts
@@ -18,6 +18,8 @@
 
 import Enmap from 'enmap';
 
+import type LexiLogger from '#lib/interfaces/LexiLogger';
+
 import type LexiClient from './LexiClient';
 import { makeVerboseFunction } from './LexiClient';
 
@@ -25,6 +27,8 @@ import { makeVerboseFunction } from './LexiClient';
  * Stores ECT (emote counter/tracker) data for Lexi.
  */
 export default class LexiECTManager {
+    private _log: LexiLogger;
+
     /** The client this belongs to. */
     public client: LexiClient;
 
@@ -35,12 +39,92 @@ export default class LexiECTManager {
     /** tildes data */
     public tildes: Enmap<string, number>;
 
-    public constructor(client: LexiClient) {
+    /** reversed version of the above data */
+    public reversed: Enmap<'uwus' | 'owos' | 'tildes', string[][]>;
+
+    public constructor(client: LexiClient, log: LexiLogger) {
         this.client = client;
+        this._log = log;
 
         this.uwus = new Enmap({ name: 'ect-uwus', verbose: makeVerboseFunction('ect-uwus') });
         this.owos = new Enmap({ name: 'ect-owos', verbose: makeVerboseFunction('ect-owos') });
         this.tildes = new Enmap({ name: 'ect-tildes', verbose: makeVerboseFunction('ect-tildes') });
+
+        this.reversed = new Enmap({ name: 'ect-reverse', verbose: makeVerboseFunction('ect-reverse') });
+        this.reversed.ensure('uwus', []);
+        this.reversed.ensure('owos', []);
+        this.reversed.ensure('tildes', []);
+        log.info('ECTManager initialized. Rebuilding reversed list...');
+        this.rebuildReversed();
+    }
+
+    public rebuildReversed() {
+        this._log.info('Verifying reversed list...');
+        const uwus = this.uwusArray();
+        const ruwus = this.reversedUwus();
+        const owos = this.owosArray();
+        const rowos = this.reversedOwos();
+        const tildes = this.tildesArray();
+        const rtildes = this.reversedTildes();
+
+        // Check uwus set
+        let uwusOk = true;
+        for (const [id, count] of uwus) {
+            if (!ruwus[count]) {
+                this._log.error(`ECTManager: uwus list is missing entry for count ${count}`);
+                uwusOk = false;
+                continue;
+            }
+            if (!ruwus[count].includes(id)) this._log.error(`ECTManager: uwus list for entry ${count} is missing id ${id}`);
+        }
+        if (uwusOk) this._log.info('ECTManager: uwus list is ok');
+        else {
+            this._log.error('ECTManager: uwus list is not ok... rebuilding.');
+            this._rebuildReverse('uwus');
+        }
+
+        // Check owos set
+        let owosOk = true;
+        for (const [id, count] of owos) {
+            if (!rowos[count]) {
+                this._log.error(`ECTManager: owos list is missing entry for count ${count}`);
+                owosOk = false;
+                continue;
+            }
+            if (!rowos[count].includes(id)) this._log.error(`ECTManager: owos list for entry ${count} is missing id ${id}`);
+        }
+        if (owosOk) this._log.info('ECTManager: owos list is ok');
+        else {
+            this._log.error('ECTManager: owos list is not ok... rebuilding.');
+            this._rebuildReverse('owos');
+        }
+
+        // Check tildes set
+        let tildesOk = true;
+        for (const [id, count] of tildes) {
+            if (!rtildes[count]) {
+                this._log.error(`ECTManager: tildes list is missing entry for count ${count}`);
+                tildesOk = false;
+                continue;
+            }
+            if (!rtildes[count].includes(id)) this._log.error(`ECTManager: tildes list for entry ${count} is missing id ${id}`);
+        }
+        if (tildesOk) this._log.info('ECTManager: tildes list is ok');
+        else {
+            this._log.error('ECTManager: tildes list is not ok... rebuilding.');
+            this._rebuildReverse('tildes');
+        }
+
+        this._log.info('ECTManager: Reversed list rebuilt.');
+    }
+
+    private _rebuildReverse(db: 'uwus' | 'owos' | 'tildes') {
+        const newArr: (string | never)[][] = [];
+        for (const [id, count] of this[db]) {
+            if (!newArr[count]) newArr[count] = [];
+            newArr[count].push(id);
+        }
+        this.reversed.set(db, newArr);
     }
 
     public forUser(id: string) {
@@ -60,19 +144,19 @@ export default class LexiECTManager {
     }
 
     public tildesForUser(id: string) {
-        return this.tildes.ensure(id, 0);
+        this.tildes.ensure(id, 0);
     }
 
     public addUwu(id: string) {
-        return this.uwus.inc(id);
+        this.uwus.inc(id);
     }
 
     public addOwo(id: string) {
-        return this.owos.inc(id);
+        this.owos.inc(id);
     }
 
     public addTilde(id: string) {
-        return this.tildes.inc(id);
+        this.tildes.inc(id);
     }
 
     public uwusArray() {
@@ -85,5 +169,17 @@ export default class LexiECTManager {
 
     public tildesArray() {
         return Array.from(this.tildes.entries());
+    }
+
+    public reversedUwus() {
+        return this.reversed.get('uwus')!;
+    }
+
+    public reversedOwos() {
+        return this.reversed.get('owos')!;
+    }
+
+    public reversedTildes() {
+        return this.reversed.get('tildes')!;
     }
 }
